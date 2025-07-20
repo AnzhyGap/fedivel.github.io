79cf3fca3c8e896e77bda8586d7237094750104c
Anzhelika <anzhy.gap@gmail.com>
2025-07-20 23:54:32 +0300

    initial commit

---
+++ script.js
@@ -0,0 +1,21 @@
+jQuery(document).ready(function ($) {
+    $('#header').load('/header.html');
+    $('#footer').load('/footer.html');
+});
+
+
+/**
+ * Всплывающий при скролле header
+ */
+//TODO FLYING HEADER
+window.addEventListener('scroll', () => {
+    const header = document.querySelector('.container_flying_header');
+
+    if (window.scrollY < 76) {
+        header.classList.add('scroll_visible');
+    } else {
+        header.classList.remove('scroll_visible');
+    }
+});
+
+/***********************************************************************************************************/
